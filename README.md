ff48d5ae76154904e814c5173113bfec90b217f3
Simão Soares <s.soares@moliporex.pt>
2019-10-21 17:50:38 +0100

    Added spring-boot-web module.

--- README.md
+++ README.md
@@ -1,3 +1,21 @@
 # Spring Boot Examples
 
 Project containing Spring Boot examples
+
+## Module list
+
+* spring-boot-data-jpa
+* spring-boot-web
+
+## Test
+
+Just type the following maven command to test all the modules.
+
+```
+mvn clean test
+```
+
+If you don't have maven installed just use the built-in maven wrapper as follows.
+```
+mvnw clean test
+```
---
+++ spring-boot-web/src/test/java/UriTemplateTest.java
@@ -0,0 +1,36 @@
+import org.junit.Test;
+import org.springframework.web.util.UriTemplate;
+
+import java.net.URI;
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Unit test featuring {@link org.springframework.web.util.UriTemplate}
+ */
+public class UriTemplateTest {
+
+    @Test
+    public void uriTemplateWithVarargsTest() {
+        short port = 8080;
+        short id = 1;
+
+        final URI expand = new UriTemplate("http://localhost:{port}/api/v1/resource/{id}")
+                .expand(port, id);
+
+        assertEquals("http://localhost:8080/api/v1/resource/1", expand.toString());
+    }
+
+    @Test
+    public void uriTemplateWithMapStringTest() {
+        UriTemplate template = new UriTemplate("https://example.com/hotels/{hotel}/bookings/{booking}");
+
+        Map<String, String> uriVariables = new HashMap<>();
+        uriVariables.put("booking", "42");
+        uriVariables.put("hotel", "Rest & Relax");
+
+        assertEquals("https://example.com/hotels/Rest%20&%20Relax/bookings/42", template.expand(uriVariables).toString());
+    }
+}
